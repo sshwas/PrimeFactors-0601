e8c1f47c73723d171abda6ce8ef3815f04684ef8
DanielKim <sshwas@gmail.com>
2023-06-01 15:29:17 +0900

    [feature] 33p

--- PrimeFactor/PrimeFactors.cpp
+++ PrimeFactor/PrimeFactors.cpp
@@ -1,5 +1,12 @@
+#include <vector>
+
+using namespace std;
+
 class PrimeFactor
 {
 public:
-
+	vector<int> of(int i)
+	{
+		return {};
+	}
 };
--- PrimeFactors-Test1/test.cpp
+++ PrimeFactors-Test1/test.cpp
@@ -1,7 +1,12 @@
 #include "pch.h"
+#include <vector>
 #include "../PrimeFactor/PrimeFactors.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+TEST(TestCaseName, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
