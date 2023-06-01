db829313f71e04fe604926afc3a55ce685313b74
DanielKim <sshwas@gmail.com>
2023-06-01 15:32:05 +0900

    [feature] 37p

--- PrimeFactors-Test1/test.cpp
+++ PrimeFactors-Test1/test.cpp
@@ -10,3 +10,10 @@ TEST(TestCaseName, Of1) {
 
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(TestCaseName, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
