9d8d7e8570f43f22e5fd585a00eb09d2d4d96cfd
DanielKim <sshwas@gmail.com>
2023-06-01 15:34:29 +0900

    [feature] 40p

--- PrimeFactors-Test1/test.cpp
+++ PrimeFactors-Test1/test.cpp
@@ -20,3 +20,8 @@ TEST_F(PrimeFixture, Of2) {
 	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
+
+TEST_F(PrimeFixture, Of3) {
+	expected = {3};
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
